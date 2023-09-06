3c9849f751cdd962132d90f3735f1684da2d0c3b
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2023-09-05 22:36:12 -0300

    parametro activity action

--- app/src/main/java/br/edu/scl/ifsp/ads/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/intents/MainActivity.kt
@@ -3,6 +3,10 @@ package br.edu.scl.ifsp.ads.intents
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.scl.ifsp.ads.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +14,10 @@ class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater) // tem que ser executado em algum momento para que meu obj seja executado
     }
+    private lateinit var parl: ActivityResultLauncher<Intent>
 
     companion object {
         const val PARAMETRO_EXTRA = "PARAMETRO_EXTRA"
-        const val PARAMETRO_REQUEST_CODE = 0
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,20 +25,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(amb.root)
         supportActionBar?.subtitle = "MainActivity"
 
+        parl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+            if (result?.resultCode == RESULT_OK) {
+                result.data?.getStringExtra(PARAMETRO_EXTRA)?.let { parametro ->
+                    amb.parametroTv.text = parametro
+                }
+            }
+        }
+
         amb.entrarParametroBt.setOnClickListener {
-            val parametroIntent: Intent = Intent(this, ParametroActivity::class.java)
+            val parametroIntent = Intent("PARAMETRO_ACTIVITY_ACTION")
             parametroIntent.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
 
-            startActivityForResult(parametroIntent, PARAMETRO_REQUEST_CODE)
-        }
-    }
+            parl.launch(parametroIntent)
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == PARAMETRO_REQUEST_CODE && resultCode == RESULT_OK){
-            data?.getStringExtra(PARAMETRO_EXTRA)?.let { parametro ->
-                amb.parametroTv.text = parametro
-            }
         }
     }
 }
