bbda62b925e97d80c274fb25d714ac04180a89a1
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2023-09-19 19:12:06 -0300

    implementação do menu_main

--- app/src/main/java/br/edu/scl/ifsp/ads/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/intents/MainActivity.kt
@@ -1,13 +1,24 @@
 package br.edu.scl.ifsp.ads.intents
 
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent.ACTION_CALL
+import android.content.Intent.ACTION_DIAL
+import android.content.Intent.ACTION_PICK
+import android.content.Intent.ACTION_VIEW
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
+import android.os.Environment
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import br.edu.scl.ifsp.ads.intents.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     // o obj só é instanciado quando ele é chamado
@@ -16,6 +27,9 @@ class MainActivity : AppCompatActivity() {
     }
     private lateinit var parl: ActivityResultLauncher<Intent>
 
+    private lateinit var permissaoChamadaArl: ActivityResultLauncher<String>
+
+    private lateinit var pegarImagemArl: ActivityResultLauncher<Intent>
     companion object {
         const val PARAMETRO_EXTRA = "PARAMETRO_EXTRA"
     }
@@ -25,10 +39,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(amb.root)
         supportActionBar?.subtitle = "MainActivity"
 
-        parl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+        parl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result?.resultCode == RESULT_OK) {
                 result.data?.getStringExtra(PARAMETRO_EXTRA)?.let { parametro ->
                     amb.parametroTv.text = parametro
+                }
+            }
+        }
+
+        permissaoChamadaArl = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            permissaoConcedida ->
+            if(permissaoConcedida){
+                chamarNumero(true)
+            } else{
+                Toast.makeText(this, "Sem permissao, sem chamada", Toast.LENGTH_SHORT).show()
+                finish()//como nao tem permissao, encerra o aplicativo
+            }
+        }
+
+        pegarImagemArl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if(result.resultCode == RESULT_OK){
+                // Uma imagem foi seleciionada
+                val imagemUri = result.data?.data
+                // se for diferente de null entra no let
+                imagemUri?.let {
+                    amb.parametroTv.text = imagemUri.toString()
+                    val visualizarIntent = Intent(ACTION_VIEW, imagemUri)
+                    startActivity(visualizarIntent)
                 }
             }
         }
@@ -40,5 +78,74 @@ class MainActivity : AppCompatActivity() {
             parl.launch(parametroIntent)
 
         }
+    }
+
+    //chamando o menu na tela main
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Kotlin nao tem switch case, tem o when()
+        return when (item.itemId){
+            R.id.viewMi -> {
+//                Toast.makeText(this, "clicou no viewMi", Toast.LENGTH_SHORT).show()
+                // fazendo abrir o navegador com a Intent
+                val url = Uri.parse(amb.parametroTv.text.toString())
+                val navegadorIntent = Intent(ACTION_VIEW, url)
+                startActivity(navegadorIntent)
+                true
+            }
+            R.id.callMi -> {
+                // verificar a versao do android
+                // se for maior que a versão 23 precisa dar permissao
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    // ver se a permissao existe
+                    if(checkSelfPermission(CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+                        chamarNumero(true)
+                    }
+                    // quando nao tem a permissao...
+                    else{
+                        // Solicitar a permissao
+                        //precisa criar outro activity result launch
+                        permissaoChamadaArl.launch(CALL_PHONE)
+                    }
+                }
+                else{
+                    //permissao ja foi dada durante a instalacao porque o android eh menor que M (23)
+                    chamarNumero(true)
+                }
+
+                true
+            }
+            R.id.dialMi -> {
+                chamarNumero(false)
+                true
+            }
+            R.id.pickMi -> {
+                val pegarImagemIntent = Intent(ACTION_PICK)
+                val diretorioImagens = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                    .path
+                pegarImagemIntent.setDataAndType(Uri.parse(diretorioImagens), "image/*")
+                // lancar a Intent
+                pegarImagemArl.launch(pegarImagemIntent)
+                true
+            }
+            R.id.chooserMi -> true
+            else -> true
+
+        }
+    }
+    private fun chamarNumero(chamar: Boolean){
+
+        val numeroUri = Uri.parse("tel: ${amb.parametroTv.text}")
+
+        val chamarIntent = Intent( if (chamar) ACTION_CALL else ACTION_DIAL)
+        chamarIntent.data = numeroUri
+        startActivity(chamarIntent)
+
+
+
     }
 }
